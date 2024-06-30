a3e7171b3fe793011d609d3f2a992a18324dbadf
Artyom Kombarov <artyom.kombaroff@gmail.com>
2024-06-30 04:00:22 +0300

    Implement some todos

--- app/Main.hs
+++ app/Main.hs
@@ -2,48 +2,65 @@ module Main where
 
 import qualified Data.Char as C
 import qualified Data.Map as M
-import qualified Data.Time as Time
+import qualified Data.Time.Clock.POSIX as P
 import qualified System.IO as IO
 import qualified System.Random as R
 
 main = do
-    handleIO    <- IO.openFile "text.txt" IO.ReadMode
-    textRaw     <- IO.hGetContents handleIO
+    handleIO <- IO.openFile "text.txt" IO.ReadMode
+    textRaw  <- IO.hGetContents handleIO
+    ts       <- truncate <$> P.getPOSIXTime
 
-    let n        = 30
-    let wMap     = wordsToMap (words $ clean textRaw) M.empty
-    let (r, gen) = R.randomR (0, (M.size wMap) - 1) (R.mkStdGen 1) :: (Int, R.StdGen)
+    let n        = 10
+    let wMap     = phrasesToMap (phrases textRaw) M.empty
+    let (r, gen) = R.randomR (0, (M.size wMap) - 1) (R.mkStdGen ts) :: (Int, R.StdGen)
     let firstW   = fst $ M.elemAt r wMap
     let result   = unwords $ buildSentence [] wMap firstW n gen
 
     putStrLn result
+
+{- https://stackoverflow.com/questions/4978578/how-to-split-a-string-in-haskell#answer-4981265 -}
+phrases :: String -> [String]
+phrases s =
+    case dropWhile isPhraseSeparator s of
+        "" -> []
+        s' -> w : phrases s''
+            where (w, s'') = break isPhraseSeparator s'
+
+isPhraseSeparator :: Char -> Bool
+isPhraseSeparator c = c `elem` ".?!,;:-[]()\n"
+
+phrasesToMap :: [String] -> M.Map String (M.Map String Int) -> M.Map String (M.Map String Int)
+phrasesToMap [] map = map
+phrasesToMap (x:xs) map
+    | xs == []  = newMap
+    | otherwise = phrasesToMap xs newMap
+        where newMap = wordsToMap (words $ prepare x) map
+
+wordsToMap :: [String] -> M.Map String (M.Map String Int) -> M.Map String (M.Map String Int)
+wordsToMap [] map = map
+wordsToMap (x:xs) map
+    | xs == []  = map
+    | otherwise = wordsToMap xs newMap
+        where newMap = M.insertWith u x subMap map
+              u      = M.unionWith (+)
+              subMap = M.fromList [(head xs, 1)]
+
+mapToWords :: M.Map String Int -> [String]
+mapToWords map = M.foldrWithKey (\w n b -> (take n $ repeat w) ++ b) [] map
+
+prepare :: String -> String
+prepare s = [C.toLower c | c <- s]
 
 buildSentence :: [String] -> M.Map String (M.Map String Int) -> String -> Int -> R.StdGen -> [String]
 buildSentence ws map w n gen = do
     if length ws == n
         then ws
         else do
-            let nextWs    = if M.member w map then (mapToWords $ map M.! w) else []
+            let nextWs    = if M.member w map then (mapToWords $ map M.! w) else [""] -- TODO_1: Find more elegant solution to..
             let (r, gen') = R.randomR (0, (length nextWs) - 1) gen :: (Int, R.StdGen)
-            let nextW     = nextWs !! (max 0 r)
+            let nextW     = nextWs !! (max 0 r) -- TODO_1: ..Fix "index too large"
 
             buildSentence (ws ++ [w]) map nextW n gen'
 
-mapToWords :: M.Map String Int -> [String]
-mapToWords map = M.foldrWithKey (\w n b -> (take n $ repeat w) ++ b) [] map
-
-clean :: String -> String
-clean s = [C.toLower c | c <- s, not $ c `elem` ".?!,;:-#[]()\"\\"]
-
-wordsToMap :: [String] -> M.Map String (M.Map String Int) -> M.Map String (M.Map String Int)
-wordsToMap (x:xs) map
-    | xs == []  = map
-    | otherwise = wordsToMap xs newMap
-    where newMap = M.insertWith u x subMap map
-          u      = M.unionWith (+)
-          subMap = M.fromList [(head xs, 1)]
-
--- TODO: Fix list range error, when text contains lonely word
--- TODO: R.mkStdGen 1 <-- replace "1" with current timestamp
--- TODO: Build wordsToMap from raw text by sentences, so it will make result lessly dumb..
--- TODO: Update clean function to replace really ALL punctuation
+-- TODO_2: Update clean function to replace really ALL punctuation, also for cyrillic
